8d545b63ef817e65349b02318722e6f4cce42512
panjikusumarizki <pnksm009@gmail.com>
2021-10-10 10:05:12 +0700

    ---

---
+++ objectFilter.js
@@ -0,0 +1,218 @@
+// const numWins = [
+//     {
+//         'BUF': '',
+//         'MIA': 9,
+//         'NE': 56,
+//         'NYJ': 1
+//     },
+//     {
+//         'SF': '',
+//         'MIA': 9,
+//         'LP': '',
+//         'NYJ': 1
+//     },
+//     {
+//         'BUF': '4',
+//         'MIA': 9,
+//         'GT': '',
+//         'NYJ': 1
+//     },
+//     {
+//         'DG': '',
+//         'MIA': 9,
+//         'PL': '',
+//         'NYJ': 1
+//     },
+//     {
+//         'ON': '',
+//         'MIA': 9,
+//         'HK': '',
+//         'NYJ': 1
+//     }
+// ];
+
+// const asArray = Object.entries(numWins);
+// const atLeast9Wins = asArray.filter(([key, value]) => value === '' || value === undefined);
+// console.log(atLeast9Wins);
+// const atLeast9WinsObject = Object.fromEntries(atLeast9Wins);
+// const keyes = Object.keys(atLeast9WinsObject);
+// console.log(keyes);
+
+let a = {
+    "crews": [
+                {
+                    "name":"Nama Awak 1",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Nakhoda",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"Y",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 2",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 3",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 4",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 5",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 6",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 7",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 8",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 9",
+                    "gender":"M",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                },
+                {
+                    "name":"Nama Awak 10",
+                    "gender":"",
+                    "birth_date":1767114000,
+                    "nationality":"ID",
+                    "sailor_book_code":123123,
+                    "end_date":1767114000,
+                    "position":"Jabatan",
+                    "sailor_code":123123,
+                    "sign_on_date":1767114000,
+                    "certificate":"N/A",
+                    "no_certificate":123123,
+                    "passport_number":123213,
+                    "passport_end_date":1767114000,
+                    "is_nakhoda":"N",
+                    "flag":0
+                }
+            ],
+    "service_number": "12369W4SKSP"
+}
+
+for (let i = 0; i < a.crews.length; i++) {
+    console.log(a.crews[i]);
+}
